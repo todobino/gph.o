cd4cf7b8184197678f5f67e4be327c97c76d4538
Brian Kimble <brian@stickbuilt.us>
2025-05-06 03:29:39 +0000

    Fix: Check for FIREBASE_PROJECT_ID before initializing Firebase Admin

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,13 +1,24 @@
 import * as admin from 'firebase-admin';
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
-  });
+try {
+  if (!admin.apps.length) {
+    if (!process.env.FIREBASE_PROJECT_ID) {
+      throw new Error('Missing FIREBASE_PROJECT_ID environment variable.');
+    }
+
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      }),
+    });
+  }
+} catch (error) {
+  console.error('Error initializing Firebase Admin:', error);
+  if (error instanceof Error) {
+    throw error
+  }
 }
 
 export const authAdmin = admin.auth();
