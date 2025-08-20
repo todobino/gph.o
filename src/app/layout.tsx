91d05f3346f98de6615f990b252c7e38d7eb1a2b
todobino <allbriandoes@gmail.com>
2025-08-20 10:01:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -23,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="overscroll-none">
       <body
         className={cn(
           'min-h-screen bg-background font-sans antialiased flex flex-col',
