425206f67847eb298ffca96f694c5c38d8df904e
todobino <allbriandoes@gmail.com>
2025-08-21 17:26:41 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <p class="text-lg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default async function Home() {
             Helping Geeks Produce for Over 40 Years
           </h1>
           <p className="text-lg md:text-xl max-w-3xl mx-auto text-primary-dark-foreground/80 mb-8">
-            Helping software teams discover, practice, and celebrate better ways of working together. Explore tidbits on TDD, refactoring, and agile teamwork.
+            My mission is to help people learn how to embrace change and harvest its value. Here you will find hundreds of free articles and videos covering software topics ranging from highly technical to broadly philosophical. If you want to learn how to create lasting change in your workplace, click the button below to book my solo or group Coaching Sessions today!
           </p>
           <div className="flex justify-center gap-4">
             <Button asChild size="lg">
