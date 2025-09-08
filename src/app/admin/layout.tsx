aa0363e4a1eefbec9cbbaebb2efbe51ff6444019
todobino <allbriandoes@gmail.com>
2025-09-08 21:29:12 +0000

    put more of a gap on the left of the icon in these menu items (_for elem

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -56,7 +56,7 @@ function AdminNav() {
                                 key={item.href}
                                 href={item.href}
                                 className={cn(
-                                    'flex items-center gap-2 rounded-md p-2 text-sm font-medium transition-colors',
+                                    'flex items-center gap-2 rounded-md px-3 py-2 text-sm font-medium transition-colors',
                                     pathname === item.href
                                     ? 'bg-primary text-primary-foreground'
                                     : 'text-muted-foreground hover:bg-accent hover:text-accent-foreground'
